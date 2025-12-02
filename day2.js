fcd9c0b89477497af98370624aac55acdd522ae4
SpeccyPlays <lilben13@googlemail.com>
2025-12-02 17:37:02 +0000

    Success for day two solution

--- day2.js
+++ day2.js
@@ -4,9 +4,29 @@ Quick solution to Day 2 of Advent of Code '25
 async function run2(inputFile){
   const ids = await getInput(inputFile);
   const convertedIds = convertInput(ids);
-  console.log(convertedIds);
+  let idCounter = processInput(convertedIds)
+  console.log(idCounter);
 }
-
+function processInput(ids){
+  let counter = 0;
+  ids.forEach(id => {
+    const min = id.min;
+    const max = id.max;
+    for (let i = min; i <= max; i++){
+      //see if we have a number we can easily split in to two
+      if (String(i).length % 2 == 0){
+        const iString = String(i);
+        const middlePoint = iString.length / 2;
+        const partA = parseInt(iString.substring(0, middlePoint));
+        const partB = parseInt(iString.substring(middlePoint));
+        if (partA === partB){
+          counter += i;
+        }
+      }
+    }
+  });
+  return counter;
+}
 async function getInput(inputFile){
   let result = await fetch(inputFile);
   if (result.ok){
@@ -22,6 +42,7 @@ function convertInput(input){
     for (let i = 0; i < input.length; i++){
         const value = String(input[i]);
         const values = value.split("-");
+        console.log(values);
         newInput.push(
             {
                 "min" : parseInt(values[0]),
