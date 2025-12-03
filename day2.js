a17e972e13a0c1368e2de3a45f1dc5698dbdbcb1
SpeccyPlays <lilben13@googlemail.com>
2025-12-03 12:37:16 +0000

    Day 2 part 2 solved....with brute force and ignorance

--- day2.js
+++ day2.js
@@ -2,12 +2,13 @@
 Quick solution to Day 2 of Advent of Code '25
 */
 async function run2(inputFile){
-  const ids = await getInput(inputFile);
-  const convertedIds = convertInput(ids);
-  let idCounter = processInput(convertedIds)
+  const ids = await getInput2(inputFile);
+  const convertedIds = convertInput2(ids);
+  let idCounter = processInput2(convertedIds)
   console.log(idCounter);
+  console.log(processInput2Part2(convertedIds))
 }
-function processInput(ids){
+function processInput2(ids){
   let counter = 0;
   ids.forEach(id => {
     const min = id.min;
@@ -27,7 +28,37 @@ function processInput(ids){
   });
   return counter;
 }
-async function getInput(inputFile){
+function processInput2Part2(ids){
+  let counter = 0;
+  ids.forEach(id => {
+    const min = id.min;
+    const max = id.max;
+    for (let i = min; i <= max; i++){
+      const iString = String(i);
+      let matchCounter = 0;
+      //This is nasty and completely brute forced but works.
+      for (let j = 0; j <= iString.length / 2; j++){
+        const partA = iString.slice(0, j);
+        if (partA === ""){
+          continue
+        }
+        for (let g = 0; g <= iString.length; g++){
+          
+          if (partA.repeat(g) === iString){
+            console.log("Number is", i, "Part A", partA.repeat(g), "Part B", iString);
+            matchCounter ++;
+            console.log("Match counter", matchCounter);
+          }
+        }
+      }
+      if (matchCounter > 0){
+        counter += i;
+      }
+    }
+  });
+  return counter;
+}
+async function getInput2(inputFile){
   let result = await fetch(inputFile);
   if (result.ok){
     const uneditedInput = await result.text();
@@ -36,13 +67,12 @@ async function getInput(inputFile){
   }
   return [];
 }
-function convertInput(input){
+function convertInput2(input){
     //convert to min/max values
     let newInput = [];
     for (let i = 0; i < input.length; i++){
         const value = String(input[i]);
         const values = value.split("-");
-        console.log(values);
         newInput.push(
             {
                 "min" : parseInt(values[0]),
