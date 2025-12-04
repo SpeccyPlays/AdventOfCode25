f3faddb8176de97e62e740afffbe44236d56b8a2
SpeccyPlays <lilben13@googlemail.com>
2025-12-04 10:23:03 +0000

    Success for day 3 part 2

--- day3.js
+++ day3.js
@@ -1,6 +1,7 @@
 async function run3(inputFile){
     const input = await getInput3(inputFile)
     console.log("Voltage is", processInput3(input));
+    console.log("Part 2 joltage is", processInput3Part2(input))
 }
 function processInput3(input){
     let voltage = 0;
@@ -18,6 +19,34 @@ function processInput3(input){
         }
         voltage += highest
     });
+    return voltage;
+}
+function processInput3Part2(input){
+    let voltage = 0;
+    
+    input.forEach(number => {
+        let voltageArray = [];
+        const strNumber = String(number);
+        let highest = 0;
+        const arrayLength = 12;
+        let removalAmount = strNumber.length - arrayLength;
+        for (let i = 0; i < strNumber.length; i++){
+            const number = parseInt(strNumber[i])
+            while (voltageArray.length > 0 && removalAmount > 0 && voltageArray[voltageArray.length -1] < number){
+                voltageArray.pop();
+                removalAmount --;
+            }
+            voltageArray.push(number);
+        }
+        voltageArray = voltageArray.slice(0, arrayLength);
+        let voltNumber = "";
+        voltageArray.forEach(digit => {
+            voltNumber += digit;
+        })
+        const fullNum = parseInt(voltNumber) || 0;
+        voltage += fullNum;
+    });
+    
     return voltage;
 }
 async function getInput3(inputFile){
