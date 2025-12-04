48907956c073252a3decf53ee88f065842f605fc
SpeccyPlays <lilben13@googlemail.com>
2025-12-04 14:52:01 +0000

    Success on day 4 part 1

--- day4.js
+++ day4.js
@@ -7,17 +7,28 @@ async function run4(inputFile){
 }
 function processInput4(input){
     let canMoveCount = 0;
-    const atLimit = 3;
+    const atLimit = 4;
     const adjacentAmount = 8;
     let spaces = [];
     for (let i = 0; i < input.length; i++){
         //why javascript, why?!
         //do below as we're going to mark with x's later
-        let markerRow = row.split("");
-        for (let j = 0; j < row.length; j++){
+        let markerRow = input[i].split("");
+        for (let j = 0; j < input[i].length; j++){
             let rollCount = 0;
             if (input[i][j] === "@"){
-                console.log("There's a @")
+                rollCount += checkForAt(input, i-1, j-1); //topLeft
+                rollCount += checkForAt(input, i-1, j); //top
+                rollCount += checkForAt(input, i-1, j+1); //topRight
+                rollCount += checkForAt(input, i, j-1); //left
+                rollCount += checkForAt(input, i, j+1); //right
+                rollCount += checkForAt(input, i+1, j-1); //bottomLeft
+                rollCount += checkForAt(input, i+1, j); //bottom
+                rollCount += checkForAt(input, i+1, j+1); //bottomRight
+                if (rollCount < atLimit){
+                    markerRow[j] = "x"
+                    canMoveCount ++;
+                }
             }
         }
         markerRow = markerRow.join("");
@@ -26,7 +37,10 @@ function processInput4(input){
     spaces.forEach(space => {
         console.log("Space", space);
     })
-    
+    return canMoveCount
+}
+function checkForAt(input, row, col){
+    return input && input[row] && input[row][col] && input[row][col] === "@" ? 1 : 0;
 }
 async function getInput4(inputFile){
   let result = await fetch(inputFile);
