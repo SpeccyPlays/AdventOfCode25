3c25a0b128500a4a2520fa2ba2cca78f4303d61d
SpeccyPlays <lilben13@googlemail.com>
2025-12-04 11:06:19 +0000

    Day 4 WIP

--- day4.js
+++ day4.js
@@ -3,7 +3,28 @@ Quick solution to Day 4 of Advent of Code '25
 */
 async function run4(inputFile){
   const input = await getInput4(inputFile);
-  console.log(input);
+  console.log("Amount of rolls that can be moved is", processInput4(input));
+}
+function processInput4(input){
+    let canMoveCount = 0;
+    let amountOfAtAllowed = 3;
+    let spaces = [];
+    for (let i = 0; i < input.length; i++){
+        const row = input[i];
+        console.log("Row is ", row);
+        let rollCount = 0;
+        for (let j = 0; j < row.length; j++){
+            if (row[j] === "."){
+                spaces.push({
+                    spaceRow : i,
+                    spaceDigit : j
+                })
+                console.log("Free space", row[j], j);
+            }
+        }
+    }
+    console.log (input);
+    console.log("Spaces are", spaces);
 }
 async function getInput4(inputFile){
   let result = await fetch(inputFile);
