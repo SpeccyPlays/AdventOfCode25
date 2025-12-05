2b3f51d5c0b69d3b2efc5ce87878e6c5f5a8bbf8
SpeccyPlays <lilben13@googlemail.com>
2025-12-05 11:49:35 +0000

    Success day 5 part 1

--- day5.js
+++ day5.js
@@ -3,8 +3,46 @@ Quick solution to Day 5 of Advent of Code '25
 */
 async function run5(inputFile) {
   let input = await getInput5(inputFile);
+  let newInput = splitInput(input);
+  console.log(processInput5(newInput));
 }
-
+function processInput5(input){
+    let freshCount = 0;
+    const available = input.available;
+    const ranges = input.ranges;
+    for (let ing = 0; ing < available.length; ing++){
+        const ingNum = parseInt(available[ing]);
+        for (let range = 0; range < ranges.length; range++){
+            const values = ranges[range].split("-");
+            const min = parseInt(values[0]);
+            const max = parseInt(values[1]);
+            if (ingNum >= min && ingNum <= max){
+                freshCount++;
+                break;
+            }
+        }
+    }
+    return freshCount;
+}
+function splitInput(input){
+    let ranges = [];
+    let available = [];
+    input.forEach(element => {
+        if (element === ""){
+            
+        }
+        else if (element.includes("-")){
+            ranges.push(element);
+        }
+        else {
+            available.push(element)
+        }
+    });
+    return {
+        "ranges" : ranges,
+        "available" : available
+    }
+}
 async function getInput5(inputFile) {
   let result = await fetch(inputFile);
   if (result.ok) {
